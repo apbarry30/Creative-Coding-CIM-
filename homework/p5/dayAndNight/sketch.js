a725d8743de2df090273d3dabb4fd90158b829ec
Amanda Barry <apbarry30@miami.edu>
2018-09-18 17:14:12 -0400

    character readmd

--- homework/p5/dayAndNight/sketch.js
+++ homework/p5/dayAndNight/sketch.js
@@ -1,6 +1,18 @@
 var sun;
 var moon;
 
+var r = 255;
+var g= 255;
+var b = 255;
+
+var cX = 50;
+var cY = 50;
+var cEdge = 20;
+
+var rectX = 50;
+var rectY = 75;
+var rectSize = 100;
+
 function preload(){
   sun= loadImage('assets/sun.png');
   moon = loadImage('assets/moon.png');
@@ -13,7 +25,8 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background (255);
+  frameRate(10);
+  background (r,g,b);
   console.log("mousex: " + mouseX + "mouseY: " + mouseY);
   //mousex: helps you differentiate and label what your doing in the console log
   //can see where the mouse is in console area and if its working
@@ -36,9 +49,11 @@ function draw() {
 
   if(true){execute this code
 
-  }
+
   */
-  if (mouseX < = width/2 ){
+
+
+  /* if (mouseX < = width/2 ){
     console.log("day Side");
     image(sun, 100, 100);
   }
@@ -49,15 +64,71 @@ function draw() {
 console.log("night side");
   image(moon, 200, 100);
 
-  }
+} i copied and pasted this again but we're changing to make it more specific**
+*/
 
 //if statements run when theyre true
 //test if statements, debug by console logs
 //know that they work correctly, then add output to interaction
 
 
-  //end of code
-}
+
 //for next class, if you wanted to put half background color, how would you do it?
 //making a new variable, and setting it change depending on what side of canvas your on
 //set variable to a color
+
+
+//add an = sign to include the mousex at 200
+//gets rid of the dead space
+//first condition and section condition have to be true to run (ALL conditions have to be true to run)
+
+if (mouseX <= width/2 && mouseX < width/2) {
+  console.log("day Side");
+  image(sun, 100, 100);
+}
+
+if (mouseX> width/2 && mouseX < width) {
+console.log("night side");
+image(moon, 200, 100);
+}
+/*do something when mouse hovers over a circluar positions
+need to see the radius of the circle, do this by a distance function
+need to know the distance the mouse is from the circle at all times (create a variable for the circle)
+distance (cX, cY, )
+*/
+
+ellipse(cX, cY, cEdge);
+// cEdge is the width and height, now we want to check the distance between the width, height and center points
+var cDist = dist (cX, cY, mouseX, mouseY);
+console.log("cDist: " + cDist);
+//prints out distance of mouse verses the circle (middle point)
+
+if (cDist < 10 ) {
+  r=255;
+  g=0;
+  b=0;
+} else {
+  r=255;
+  g=255;
+  b=255;
+}
+// ^ if we are inside of the circle
+
+
+if (mouseX > rectX && mouseX < rectX + rectSize){
+  if (mouseY > rectY && mouseY < rectY + rectSize){
+    console.log ("in here");
+  }
+//same way of writing the same if statement in one statement
+}
+if(mouseX > rectX && mouseX < rectX + rectSize && mouseY > rectY && mouseY < rectY + rectSize){
+  console.log ("in here");
+  //using the random function (really from 0-255)
+  r = random(256);
+  g = random (0-256);
+  b= random (256);
+}
+fill (r,g,b);
+rect(rectX, rectY, rectSize, rectSize);
+  //end of code
+}
--- homework/p5/monkey week3/sketch.js
+++ homework/p5/monkey week3/sketch.js
@@ -1,7 +1,7 @@
 var facePosX= 500;
 var facePosY= 500;
-var eyeWidth= 100;
-var eyeHeight= 200;
+var eyeWidth= 80;
+var eyeHeight= 120;
 
 function setup() {
   createCanvas(1000,1000);
@@ -11,34 +11,68 @@ function setup() {
 
 function draw() {
   background ('white');
-  facePosX = mouseX;
-  facePosY = mouseY;
+
 // put drawing code here
   stroke('black');
-  strokeWeight(2);
+  strokeWeight(5);
 
 
-  fill('brown');
-  ellipse(facePosX,facePosY,500,400);
+  fill('white');
+  ellipse(facePosX-20,facePosY-10,500,400);
   //face
+  fill('blue');
+  ellipse(facePosX-100,facePosY-75,eyeWidth+20,eyeHeight+20);
+
+  ellipse(facePosX+75,facePosY-75, eyeWidth+20, eyeHeight+20);
   fill ('white');
-  ellipse(facePosX-50,facePosY,eyeWidth,eyeHeight);
+  ellipse(facePosX-100,facePosY-75,eyeWidth,eyeHeight);
   //eye
-  ellipse(facePosX+50,facePosY, eyeWidth, eyeHeight);
+  ellipse(facePosX+75,facePosY-75, eyeWidth, eyeHeight);
   //eye
-  fill ('black');
-  rect(facePosX ,facePosY+200,180,20);
+
+  //eye
+
+
+  fill ('red');
+  ellipse(facePosX-10, facePosY+10, 75, 75);
+  //nose
+  fill ('red');
+  rect(facePosX-100 ,facePosY+100,180,20);
   //mouth
 
 
   stroke('black');
-  fill('brown');
+  fill('red');
   strokeWeight(5);
-  point(eyeWidth/2,eyeHeight/2);
+  point(facePosX,facePosY);
 
 
-  line(facePosX,facePosY+210,facePosX + 180,facePosY+210);
+  line(facePosX-100,facePosY+110,facePosX +80,facePosY+110);
+  line(facePosX-110,facePosY+100, facePosX-100, facePosY+110);
+  line(facePosX +80,facePosY+110, facePosX+100, facePosY+100);
 
-  arc(100,200,100,100,QUARTER_PI, PI+ HALF_PI+ QUARTER_PI);
+  arc(facePosX-10,facePosY-160,300,300,PI,0);
+
+  facePosX = mouseX;
+  facePosY = mouseY;
+  fill('black');
+  ellipse(facePosX-100,facePosY-50,eyeWidth-50,eyeHeight-50);
+  ellipse(facePosX+80,facePosY-50,eyeWidth-50,eyeHeight-50);
+//eyeballs
+facePosX=500;
+facePosY=500;
+if(mouseX < facePosX){
+console.log ("in here");
+//using the random function (really from 0-255)
+r = random(256);
+g = random (0-256);
+b= random (256);
+
+}
+fill (r,g,b);
+ellipse(facePosX, facePosY, 100, 100);
+
+
+
 
 }
