7f7d144206404b89f31ca9463b4a6ff1591938bb
Amanda Barry <apbarry30@miami.edu>
2018-09-27 18:09:55 -0400

    commit

--- homework/dayAndNight9_18_18/sketch.js
+++ homework/dayAndNight9_18_18/sketch.js
@@ -2,6 +2,13 @@ var curImage, sun, moon;
 
 var selection, textInput, submitButton;
 
+//var posX = [0,100,200,300];
+//var posY = [0,100,200,300];
+//creating an array just with x and y position and keep width and height static
+var posX = [];
+var posY = [];
+
+
 var numberOfImages = 0;
 
 function preload(){
@@ -39,6 +46,17 @@ submitButton.mousePressed(function(){
 }  else if (selection.value()== "moon"){
   curImage = moon;
   }
+
+  posX = [];
+  posY= [];
+  //resetting and emptying array to use again.clearing and adding to position. 
+  for (var a = 0; a < numberOfImages; a ++ ){
+    posX [a] = random(width);
+    posY [a] = random(height);
+    console.log ("x: " + posX[a] + "y: " + posY[a]);
+
+  }
+
 })
 
 curImage= sun;
@@ -46,12 +64,21 @@ curImage= sun;
 
 function draw() {
 
-frameRate(5);
+//frameRate(5);
   background(255);
-  for(var i = 0; i <numberOfImages; i ++){
+/*  for(var i = 0; i <numberOfImages; i ++){
     console.log (i);
   image (curImage, random(width),random(height), curImage.width/random(1.5), curImage.height/random(1.5));
   }
+  for randomized repeating images with the number of images as the number the user entered
+*/
+
+for(var i = 0; i< numberOfImages; i ++){
+image (curImage, posX[i], posY[i], curImage.width/4, curImage.height/4);
+//posX.length (# of items in array) represents 4 because we have 3 items in array and start from 0 and need i < 4
+
+}
+
   //draw once per loop (draw loop)
 //but for loop with run 10 times
 
@@ -65,4 +92,4 @@ takes a variable (with a number), condition (how many times want to run), and it
 */
 
 //homework is to add 2 interactivity methods to character
-//change background, fill, mousePressed, hitzone, using buttons, text input etc. 
+//change background, fill, mousePressed, hitzone, using buttons, text input etc.
---
+++ homework/frameAnimationStart/sketch.js
@@ -0,0 +1,48 @@
+var frameAmounts =4;
+var frameArray = [];
+var currentFrame = 0
+
+var interval = 2000;
+//2,000 seconds
+var prevMillis = 0
+
+function preload(){
+  //var frameString = "assests/Thatwasclose0.jpg";
+  //string representing path where the image is on our computer
+  //frameArray[0] = loadImage(frameString);
+
+  for(var frames = 0; frames < frameAmounts; frames ++ ){
+    var frameString = "assests/Thatwasclose0.jpg" + frames + ".jpg";
+    //string representing path where the image is on our computer
+    frameArray[frames] = loadImage(frameString);
+    //new path to image, loads it to corresponding place in array
+    //we do this instead of doing.. frameArray [1]= loadImage(...) each seperately
+
+  }
+}
+
+function setup(){
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  //frameRate(1);
+  //console.log(millis());
+  image(frameArray[currentFrame],0,0);
+
+  if(millis()- prevMillis > interval) {
+    currentFrame ++;
+    prevMillis = millis();
+    //save state where last ran and resets 
+  }
+
+  //console.log(currentFrame);
+  if (currentFrame> frameAmounts-1){
+    currentFrame = 0;
+    //increading currentFrame AFTER displaying the image
+  }
+   ellipse (mouseX,mouseY, 10,10);
+}
+
+//end
